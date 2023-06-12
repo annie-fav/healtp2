05423b6d4a62831dd3d5ccd2ec9119ad6553e9e4
annie <fav.hala@gmail.com>
2023-06-12 14:55:29 -0300

    init-commit

---
+++ src/Componentes/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+         <div className="italic font-serif mt-4 ml-12 mr-12 text-justify indent-6">
+          <h3 className="text-xl font-bold text-pink-800">Bibliografia</h3>
+          <br></br>
+          <ul className="text-left ml-2 text-justify">
+            <li>* CATTARUZZA, A (2009) "Otras dimensiones de la experiencia peronista" en Historia Argentina 1916-1955,  Buenos Aires, Siglo XXI p: 229-250 . *</li>
+            <li>* CARLI, S. (2002) “Los niños entre los derechos y la política. Peronismo, Pedagogía y Transformaciones sociales (1945- 1955), en : Niñez, pedagogía y política. Buenos Aires,  Miño y Dávila..</li>
+            <li>* BUCHBINDER, P. (2005) “La Renovación Universitaria 1955-1966”, en: Historia de las Universidades Argentinas. Buenos Aires, Sudamericana..</li>
+            <li>* KAUFMANN, C. Y DOVAL, D. (1997), “Cap. 2: Libros aprobados – Libros prohibidos – Libros recomendados, Argentina 1976-1982”, en: Una pedagogía de la renuncia. El Perennialismo en la Argentina 1976-1983, UNER, Paraná, pp. 45-70..</li>
+            <li>* INVERNIZZI, H. GOCIOL J. (2007) "Prólogo", "Marco general", en Un golpe a los libros. Represión a la cultura durante la última dictadura militar. Eudeba. Buenos Aires, pp. 15-74 .</li>
+            <li>* FLORES, J; ARAUJO, J Y DABAT, R (2010) Política y Educación: Un vínculo necesario en el debate nacional, en: LUGONES, G Y FLORES, J (comp) Intérpretes e interpretaciones de la Argentina en el bicentenario, Bernal, UNQ, pp. 97-104</li>
+            <li>* FILMUS, D. (1996), “Cap. 3. Principales tendencias en la evolución actual del Sistema Educativo Argentino” en Estado, sociedad y educación en la Argentina de fin de siglo, Buenos Aires, Troquel, pp. 47- 99.</li>
+          </ul>
+          <hr className="my-8 h-1 h-px bg-rose-300 h-px border-0"></hr>
+          <p className="text-right italic mr-20 font-bold">Florencia Anabel Vazquez</p>
+          </div>
+          <hr className="mt-10"></hr>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Componentes/Main.jsx
@@ -0,0 +1,180 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <>
+            <div className="font-serif mt-4 mx-12 text-justify indent-6">
+                <hr className="mt-10"></hr>
+                <h4 className="italic mt-10 ml-4 text-lg font-bold">¿Cuál es el contexto sociopolítico de la segunda posguerra a nivel mundial?</h4>
+                <br></br>
+                <div className="mx-24 inline-flex">
+                    <div>
+                        <p className="font-bold text-pink-800">IA:</p>
+                        <p>El contexto socio-político de la segunda posguerra a nivel mundial estuvo caracterizado por el enfrentamiento
+                            entre Estados Unidos y la Unión Soviética, esta división se conoce como la Guerra Fría que duró desde 1940 hasta
+                            principios de la década de 1990. Luego la IA describe brevemente la situación de cada uno. Una vez realizado
+                            esto, menciona otros aspectos importantes del contexto sociopolítico de la posguerra los cuales incluyen
+                            brevemente: la descolonización, la construcción de instituciones internacionales, los derechos humanos y
+                            desafíos sociales, y la reconstrucción y desarrollo económico. Omitiendo palabras claves como estado de
+                            bienestar y estado liberal, sindicatos, y organizaciones empresariales.</p>
+                        <br></br>
+                        <p className="font-bold text-pink-800">Bibliografia</p>
+                        <p>El contexto socio-político de la segunda posguerra a nivel mundial estuvo caracterizado por el enfrentamiento
+                            entre Estados Unidos y la Unión Soviética, enfrentándose dos líneas de pensamiento vigentes de la época: el
+                            capitalismo y el socialismo. En consecuencia a este conflicto mundial, se hizo necesario que a nivel
+                            internacional se creen nuevas pautas con el fin de establecer una reconstrucción económica y social a nivel
+                            mundial. Estas nuevas medidas establecidas plantearon dos características principales, por un lado se construyó
+                            una economía basada fuertemente en el sistema capitalista, el cual comprende su mayor crecimiento económico a
+                            nivel global entre los periodos de 1940 y 1970. Por otro parte, es el Estado y su protagonismo en cuanto a
+                            intervenir activamente en los aspectos sociales y económicos de la sociedad, lo que lo caracteriza en este nuevo
+                            contexto sociopolítico, surgiendo así el Estado de Bienestar durante el fin de la segunda guerra mundial, y
+                            dejando de este modo atrás al modelo de Estado liberal que existía hasta ese entonces. De esta manera el
+                            desarrollo económico y el bienestar social son los principales intereses no solo del estado sino también que
+                            comienzan a tener gran participación activa los sindicatos y las organizaciones empresariales.</p>
+                        <p>En cuanto a la región de latinoamérica, se hizo evidente luego de la finalización de la Segunda Guerra Mundial,
+                            las limitaciones que empezaron a hacerse visibles en cuanto al proceso de industrialización y la necesidad de
+                            incorporar las nuevas tecnologías para que el modelo se manifieste con éxito.</p>
+                    </div>
+                    <div>
+                        <img className="mt-5 mt-40 ml-10 mr-60" src="https://concepto.de/wp-content/uploads/2020/12/guerra-fria-e1608065920563.jpg" />
+                    </div>
+                </div>
+                <hr className="mt-10"></hr>
+                <div className="mx-24 inline-flex">
+                    <div>
+                        <h4 className="italic mt-10 ml-4 text-lg font-bold">¿Qué contexto político existía en la Argentina luego de la segunda posguerra?</h4>
+                        <br></br>
+                        <p className="font-bold text-pink-800">IA:</p>
+                        <p> El contexto político de Argentina después de la Segunda Guerra Mundial estuvo identificado por la presencia de
+                            Juan Domingo Peron en la presidencia, creando lo que se denominó “peronismo”, e implementó una serie de
+                            políticas populistas y nacionalistas. Tuvo amplio apoyo de la clase trabajadora y los sindicatos. Adopto sin
+                            embargo un enfoque autoritario y concentró gran parte del poder político y económico en el estado. Además de que
+                            estuvo caracterizado por una gran inestabilidad política y los cambios de gobierno que se produjeron en las
+                            décadas siguientes. Omitiendo una descripción más detallada de lo que implica el Estado de Bienestar
+                            caracterizado de aquella época.</p>
+                        <br></br>
+                        <p className="font-bold text-pink-800">Bibliografia</p>
+                        <p> En Argentina, fue el proyecto político peronista el que asumió el poder en 1946. Sus propuestas se encontraban
+                            en torno al rol activo del Estado en cuanto a intervenir en el sector económico, político y social de la
+                            sociedad de la época. Su rol protagónico permitió por primera vez la participación activa política y social de
+                            grandes masas de la sociedad, dejando de ser exclusivo solo a unos pocos. Este modelo de Estado se caracterizó
+                            por tener una industrialización centrada en la sustitución de importaciones en una economía semicerrada, a su
+                            vez postulaba cumplir una función reguladora en la economía interviniendo y participando en la producción.
+                            Además cumple el rol de ser distribucionista, brindando asignaciones familiares y sociales para cubrir distinto
+                            tipo de riesgos y necesidades a nivel social y funcionar como un medio para solucionar los conflictos entre las
+                            clases sociales. De esta manera, el peronismo a nivel económico lleva a cabo una fuerte política de
+                            nacionalización de empresas y servicios que hasta entonces se encontraban en posesión extranjera, mientras que a
+                            nivel social se proclama garante de los derechos sociales, procurando impartir justicia social.
+                        </p>
+                    </div>
+                    <div>
+                        <img className="mt-5 mt-60 ml-10 mr-60" src="https://cloudfront-us-east-1.images.arcpublishing.com/infobae/QOLFJGZLBRH7DJ3JHOD56EY6G4.jpg" />
+                    </div>
+                </div>
+                <hr className="mt-10"></hr>
+                <div className="mx-24 inline-flex">
+                <div>
+                <h4 className="italic mt-10 ml-4 text-lg font-bold">¿Cuáles fueron los objetivos principales con respecto al sistema educativo argentino durante el gobierno de
+                    Peron?</h4>
+                <br></br>
+                <p className="font-bold text-pink-800">IA:</p>
+                <p>Durante el gobierno de Perón se establecieron ciertos objetivos en relación al sistema educativo. Estos objetivos
+                    reflejaban la visión y los principios del peronismo y buscaba transformar la educación en un instrumento para
+                    promover la justicia social, el nacionalismo y el desarrollo económico del país. Algunos de esos objetivos eran:
+                    universalización de la educación, formación técnica y profesional, educación para el trabajo y la producción,
+                    justicia social y equidad educativa, formación integral y valores peronistas. Omitiendo conceptos como por
+                    ejemplo democratización social, Primer Plan Quinquenal, Ley 12.978, iglesia y creación de jardines.
+                </p>
+                <br></br>
+                <p className="font-bold text-pink-800">Bibliografia</p>
+                <p> Uno de los objetivos fundamentales para que gran parte de las medidas peronistas lograrán materializarse, giró
+                    entorno a la importancia de formar al ciudadano, diferenciándose de aquel concepto de ciudadano del modelo
+                    político anterior al peronismo, el cual mantenía las características particulares de formar a un ciudadano con
+                    características que respondiera al contexto sociopolítico del modelo liberal, caracterizado por considerar la
+                    niñez como sujeto universal y de derecho, y considerando la expansión de la escolaridad pública.</p>
+                <p>Para el peronismo, en cambio, el concepto de niñez representaba a las futuras generaciones de la sociedad, por
+                    ello consideraban muy importante la intervención del Estado en cuanto a la constitución de la identidad de los
+                    mismos, colocando su foco en la pobreza infantil y de su condición popular para constituir un nuevo sujeto
+                    político cuya identidad se definiera, ya no solo por sus derechos propios, sino por su pertenencia al nuevo
+                    territorio de la nación. Así, el peronismo diseñó una política denominada democratización social que respondía a
+                    un conjunto de demandas sociales históricas, enunciando al Estado como su garante absoluto.</p>
+                <p>De esta manera, la política educativa peronista desarrollo varios proyectos y leyes, una de las primeras medidas
+                    fue el denominado Primer Plan Quinquenal (1946), el cual se centró en la cuestión de la “igualdad de
+                    oportunidades”, postulando la enseñanza primaria “obligatoria, gratuita, y gradual”, de los 5 a los 14 años,
+                    concibiendo una educación democrática que permitiera el acceso a la educación a toda la sociedad, teniendo como
+                    objetivo el logro de un orden de justicia social, entendida como una construcción social. También es sancionada
+                    la Ley 12.978 en 1947, dándole continuidad al decreto de la enseñanza religiosa, postulando que la educación
+                    escolar debía tener contenidos religiosos. Además, el peronismo propone la creación de jardines con el fin de
+                    garantizar un orden social homogéneo para la construcción de una nueva generación infantil, planteando la
+                    expansión del nivel inicial, y permitiendo unir las demandas pedagógicas con las necesidades políticas y
+                    sociales del Estado que requerían una institución funcional para la inserción de la mujer en el mundo laboral
+                    (Carli, S. 2002).</p>
+                <p>En palabras de Cattaruzza (2009), podemos observar que entre 1946 y 1950 el gobierno peronista intentó impulsar
+                    sus propias políticas culturales, intentando garantizar el acceso a un conjunto de productos culturales
+                    considerados importantes, objetivo que se alineaba con la ampliación del acceso a la educación, y en un marco
+                    más general, en promover la mejora de la calidad de vida de la sociedad.
+                </p>
+                </div>    
+                <div>
+                <img className="mt-5 mt-80 ml-10 mr-60" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaLpyRuuw9ZEpdnXbCN1kLH2v5FDMedhuESg6tmO5wrKkE7OTlJjhYXlUh7bxcz1PQQz8&usqp=CAU"></img>
+                </div>
+                </div>
+                <hr className="mt-10"></hr>
+                <div className="mx-24 inline-flex">
+                <div>
+                <h4 className="italic mt-10 ml-4 text-lg font-bold">¿Qué sucede luego del derrocamiento de Peron y qué cambios surgen en el sistema educativo argentino?
+                </h4>
+                <br></br>
+                <p className="font-bold text-pink-800">IA:</p>
+                <p>Luego del derrocamiento de Peron en Argentina en 1955 se instauró un periodo de inestabilidad política y cambios
+                    significativos en el país. Durante este periodo, conocido como “Revolución Libertadora” se llevaron a cabo una
+                    serie de medidas que afectaron al sistema educativo argentino, como por ejemplo un enfoque educativo más
+                    conservador, cambios en los programas de estudios, y una mayor influencia de las instituciones religiosas en la
+                    educación, basado en valores tradicionales y religiosos. También hubo persecuciones docentes, para
+                    “desperonizar” las instituciones educativas.</p>
+                <p>La IA hace un pequeño resumen, mientras que en la bibliografía por ejemplo podemos comprender con más detalle
+                    qué ocurre con las universidades, que pasó luego de la revolución libertadora con el retorno de Peron, y luego
+                    el golpe de Estado de 1976. Mencionando sólo brevemente que hubo reformas y contrarreformas durante todo este
+                    periodo.</p>
+                <br></br>
+                <p className="font-bold text-pink-800">Bibliografia</p>
+                <p> Sin embargo, en 1955 el gobierno peronista sufre un golpe militar, quedando en ese entonces el poder en manos
+                    del gobierno que se denominaba: la Revolución Libertadora. Este gobierno de facto tuvo enormes intenciones de
+                    desperonizar toda institución académica, con la intención de avanzar en una dirección opuesta a la del
+                    peronismo. Así, por ejemplo el sistema universitario quedó en manos de un heterogéneo grupo que buscaban
+                    reconstruir el sistema con los mismos lineamientos con los que había funcionado durante las décadas del veinte.
+                    (Buchbinder, P. 2005).</p>
+                <p> Hacia 1958 vuelve el mecanismo constitucional de elección presidencial, en el cual asume al gobierno Arturo
+                    Frondizi. Sin embargo, los problemas políticos y económicos heredados de la Revolución Libertadora, llevaron a
+                    un nuevo golpe de Estado. El modelo autoritario que se desarrolló en Argentina a finales de la década del ‘60,
+                    derivó en el denominado Proceso de Reorganización Nacional, en el cual se postula una modificación radical de la
+                    política y de la sociedad. Sin embargo, en la década del ‘70 el movimiento peronista retorna al poder. El nuevo
+                    gobierno de Peron se apoyaba en tres pilares, un acuerdo democratico con las principales fuerzas políticas, un
+                    pacto social con las corporaciones económicas y una conducción centralizada del movimiento peronista tendiente a
+                    superar las fuertes divisiones internas que lo dividían. Pero con la muerte de Peron en julio de 1974 se entró
+                    en una nueva fase del gobierno peronista en la cual los actores modificaron la raíz de su estrategia, viéndose
+                    afectado principalmente el sector social. Es así como el 24 de marzo de 1976 un nuevo golpe institucional sucede
+                    en la nación Argentina conducido por las Fuerzas Militares. El principal objetivo era el de “ordenar” a la
+                    sociedad, según el criterio de las Fuerzas Armadas, proponiendo un “disciplinamiento social”, y utilizando como
+                    medios la represion y el terrorismo de Estado. El proyecto educativo pensado por las fuerzas autoritarias
+                    implicó lo que se denominó la militarización del sistema educativo, en donde se incluyó la participación de los
+                    militares en espacios que eran tradicionalmente ocupados por civiles, aludiendo a prácticas autoritarias en los
+                    ámbitos educacionales. El sistema educativo era un lugar fundamental para los militares en cuanto pretendieron
+                    ejercer su adoctrinamiento a través de este, planteando una represión simbólica, fundamentada en una filosofía
+                    de la educación personalista, contribuyendo a consolidar un estilo educativo capaz de imponer una homogeneidad
+                    ideológica y pedagógica establecida en una tradición conservadora que permitía ocultar los conflictos sociales.
+                    (Kaufmann, C. 1997). A su vez se desarrolló una política de control y censura sobre libros de textos que no
+                    contuviera, en palabras de Invernizzi, H. y Gociol J. (2007), “connotaciones ideológicas contrarias al ser
+                    nacional”.</p>
+                    </div>
+                    <div>
+                    <img className="mt-80 ml-10 mr-60" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrsfolygwq9xcD2P33X87xm3urV_pina7z0A&usqp=CAU"></img>
+                    </div>
+                    </div>    
+                <hr className="mt-10"></hr>
+            </div>
+        </>
+    )
+}
+
+export default Main
